7d9b8a1188ac6bbe8f15573bba02f51e89afcd9c
TiagoM13 <thyagomotha2000@gmail.com>
2022-05-11 21:04:47 -0300

    update api

--- src/components/Transaction/index.tsx
+++ src/components/Transaction/index.tsx
@@ -4,9 +4,8 @@ import { Container } from "./styles"
 
 export const Transaction = () => {
   useEffect(() => {
-    fetch("http://localhost:3000/api/transactions")
-      .then(response => response.json())
-      .then(data => console.log(data))
+    api("/transactions")
+      .then(response => console.log(response.data))
   }, []);
 
   return (
